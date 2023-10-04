de8ebd277065d37430a0e135bef76e8e82b8c96c
Álvaro Jiménez Ponce <115788430+alvarojimeenez@users.noreply.github.com>
2023-10-04 18:43:42 +0200

    Update calculator

--- src/main/java/calculadoraExtrania/Calculator.java
+++ src/main/java/calculadoraExtrania/Calculator.java
@@ -1,48 +1,42 @@
 package calculadoraExtrania;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class Calculator {
+public class Calculator{
 	
 	String cadena; 
 	
 	public Calculator(String cadena) {
-		this.cadena = cadena;
+			this.cadena = cadena;
 	}
 	
 	public String operar() {
-		int result = 0;
+		double result = 0;
 		String numberCadena = "";
 		String operator = "";
-		
-		for (int i = 0; i<this.cadena.length(); i++) {
-			if (Character.isDigit(this.cadena.charAt(i))) {
-				numberCadena+=String.valueOf(this.cadena.charAt(i));
-			}
-			
-			else if (!Character.isDigit(this.cadena.charAt(i)) && operator.isEmpty()){
-				result+=Integer.parseInt(numberCadena);
-				operator+=String.valueOf(this.cadena.charAt(i));
-				numberCadena = "";
-			}
-			else{
-				if (operator.equals("+")){
-					result+=Integer.valueOf(numberCadena);
-				}else {
-					result-=Integer.valueOf(numberCadena);
+		if (validarCadena()==true) {
+			for (int i = 0; i<this.cadena.length(); i++) {
+				if (Character.isDigit(this.cadena.charAt(i)) || cadena.charAt(i)=='.' || (cadena.charAt(i)== '-' && i==0)) {
+					numberCadena+=String.valueOf(this.cadena.charAt(i));
 				}
-				operator = "";
-				numberCadena = "";
+				else if (!Character.isDigit(this.cadena.charAt(i)) && operator.isEmpty()){
+					result+=Double.valueOf(numberCadena);
+					operator+=String.valueOf(this.cadena.charAt(i));
+					numberCadena = "";
+				}
+				else{
+					if (operator.equals("+")){
+						result+=Double.valueOf(numberCadena);
+					}else {
+						result-=Double.valueOf(numberCadena);
+					}
+					operator = "";
+					numberCadena = "";
+				}
 			}
-		}
-		if (operator.equals("+")){
-			result+=Integer.valueOf(numberCadena);
-		}else {
-			result-=Integer.valueOf(numberCadena);
+			if (operator.equals("+")){
+				result+=Integer.valueOf(numberCadena);
+			}else {
+				result-=Integer.valueOf(numberCadena);
+			}
 		}
 		return String.valueOf(result);
 	}
@@ -51,4 +45,16 @@ public class Calculator {
 		return "";
 	}
 	
+	public boolean validarCadena() {
+		boolean result = true;
+		for (int i = 0; i<this.cadena.length(); i++) {
+			if ((this.cadena.charAt(i)== '+' && this.cadena.charAt(i+1)== '+') 
+				|| (this.cadena.charAt(i)== '+' && this.cadena.charAt(i+1)== '-') 
+				|| (this.cadena.charAt(i)== '-' && this.cadena.charAt(i+1)== '+') 
+				|| (this.cadena.charAt(i)== '-' && this.cadena.charAt(i+1)== '-')) {
+				result = false;
+			}
+		}
+		return result;
+	}
 }
--- src/main/webapp/calculadoraExtrania.jsp
+++ src/main/webapp/calculadoraExtrania.jsp
@@ -6,10 +6,12 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
+<link rel="stylesheet" href="css/estilos.css" type="text/css">
 </head>
 <body align="center">
 <%! 
 String cadenaNumeros = "";
+String almacenarResultado = "";
 %>
 
 <%
@@ -19,29 +21,41 @@ String cadenaNumeros = "";
 	String number = request.getParameter("number");
 	String operador = request.getParameter("operator");
 	
-	
 	Calculator c = new Calculator(cadenaNumeros);
-
 	if (send!=null){
+		if (almacenarResultado.isEmpty()){
 		cadenaNumeros+=number+operador;
+		}else{
+		cadenaNumeros = almacenarResultado;
+		cadenaNumeros+=number+operador;
+		almacenarResultado ="";
+		};
 	}
 	if(calculate!=null){
-		cadenaNumeros= c.operar();
+		if (cadenaNumeros!=null && c.validarCadena()==true && !cadenaNumeros.isEmpty()){
+			almacenarResultado = c.operar();
+			cadenaNumeros= cadenaNumeros+"=" + almacenarResultado;		
+		}else {
+			cadenaNumeros = "Error";
+			almacenarResultado = "";
+		}
 	}
 	if (delete!=null){
 		cadenaNumeros= c.borrar();
 	}
+	
+
 
 %>
 	<form action="calculadoraExtrania.jsp" method="post">
 		<h1>Calculator</h1>
-	<textarea> <%= cadenaNumeros %></textarea>
-		<br>
-		<label>Number: <input type="number" name="number"></label>
+		<input class="resultado" value="<%= cadenaNumeros %>"></input>
+		<br><br><br>
+		<label>Number: <input type="number" step="any" name="number"></label>
 		<br><br>
-		<label>Introduce signo:</label>
+		<label class="signo">Sign:</label>
 		<select name="operator">
-			<option value="">Sign</option>
+			<option value=""></option>
 			<option value="+">+</option>
 			<option value="-">-</option>
 		</select>
@@ -49,6 +63,7 @@ String cadenaNumeros = "";
 		<input type="submit" name="send" value="Send">
 		<input type="submit" name="calculate" value="Calculate">
 		<input type="submit" name="delete" value="Delete">
+		<br><br>
 	</form>
 
 </body>
